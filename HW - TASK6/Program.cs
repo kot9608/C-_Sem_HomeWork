9d0bee0bce5fee436ba15a5770232ca9994e1b68
Kirill <Kot9608@mail.ru>
2023-01-28 16:41:37 +0500

    HW2 - Task6

---
+++ HW - TASK6/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число и выдает, 
+//является ли число четным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+int ost = (number1%2);
+if (ost==0) {
+  Console.WriteLine($"Число {number1} чётное");
+}
+else if (ost==1) {
+  Console.WriteLine($"Число {number1} нечётное");
+}
