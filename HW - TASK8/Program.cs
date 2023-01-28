cb5a52459020267b919e97f64f3662b65d1f63ca
Kirill <Kot9608@mail.ru>
2023-01-28 17:18:38 +0500

    HW2 - Task8

---
+++ HW - TASK8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= number1)
+{
+    if (i%2==0)
+    {
+      Console.WriteLine(i);
+    }
+    i=i+1;
+};
